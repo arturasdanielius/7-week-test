9ef3d131df031d00a6280913ad15ad860e7b7168
Artūras Danielius <arturas.danielius@gmail.com>
2022-07-15 15:02:29 +0300

    jau po testo sprendziam toliau.

--- test.js
+++ test.js
@@ -8,7 +8,7 @@ if(a > b && a != b){
     return a + ' a - didesnis skaicius';
     }else if (b > a && b != a) {
     return b + ' b - didesnins skaicius';
-    }else if (a === b) {
+    }else{
     return 'a = b'
     }
 }
@@ -18,7 +18,7 @@ console.log(palyginus);
 
 console.log('----------2---------');
 //2. Naudojant for ciklą, išvesti į konsolę skaičius nuo 1 iki 10. (5 taškai)//
-let i;
+
 for(let i = 1; i < 11; i++){
     console.log(i);
 } 
@@ -51,11 +51,67 @@ console.log('----------5---------');
 
 //5. Naudojant while ciklą, spausdinti atsitiktinius skaičius nuo 1 iki 10. Paskutinis atspausdintas skaičius turi būti 5. (7 taškai)
 
-/*6. Sukurti masyvą, kurio ilgis būtų nuo 20 iki 30, o reikšmės būtų skaičiai nuo 10 iki 30. Surasti didžiausią masyvo reikšmę, NENAUDOJANT sort() bei Math.max() funkcijų. (7 taškai)
-7. Sugeneruokite masyvą, kurio reikšmės atsitiktinės raidės A, B, C ir D, o ilgis 100. Suskaičiuokite kiek yra kiekvienos raidės. (7 taškai)
-8. Parašyti funkciją - lygineSuma. Funkcijos parametrai - du kintamieji. Testų reikalavimai - abu kitamieji turi būti arba skaičiai arba masyvai(negali būti vienas skaičius, kitas masyvas).
-Jei kintamieji skaičiai, grąžinti skaičių sumą, jei kintamieji masyvai - grąžinti masyvų ilgių sumą. Jei abu kintamieji skaičiai arba masyvai, bet suma nelyginė - grąžinti tekstą, kad suma nelyginė. (10 taškų)
-9. Parašyti funkciją pirminisSkaicius. Funkcija turi vieną kintamąjį. Turi būti patikrinimas, kad kintamasis yra skaičius. Funkcija turi grąžinti ar pateiktas skaičius yra pirminis( pirminis
+let c = 0;
+while(c !== 5){
+    c = rand(1, 10);
+    console.log(c);
+}
+
+console.log('----------6---------');
+//6. Sukurti masyvą, kurio ilgis būtų nuo 20 iki 30, o reikšmės būtų skaičiai nuo 10 iki 30. Surasti didžiausią masyvo reikšmę, 
+//NENAUDOJANT sort() bei Math.max() funkcijų. (7 taškai)
+
+let maxvalue = 0;
+const masyvas = [];
+for (let i = 0; i < rand(20, 30); i++){
+    masyvas.push(rand(10, 30));
+    if(masyvas[i] > maxvalue){
+        maxvalue = masyvas[i];
+    }
+}
+console.log(masyvas);
+console.log(maxvalue);
+
+console.log('----------7---------');
+//7. Sugeneruokite masyvą, kurio reikšmės atsitiktinės raidės A, B, C ir D, o ilgis 100. Suskaičiuokite kiek yra kiekvienos raidės. (7 taškai)
+
+const nmas = [];
+let raides = ['A', 'B', 'C', 'D'];
+for(let i = 0; i < 100; i++){
+    nmas.push(raides[rand(0, 3)]);
+}
+let kiekis1 = 0;
+let kiekis2 = 0;
+let kiekis3 = 0;
+let kiekis4 = 0;
+for (let i = 0; i < nmas.length; i++){
+    if (nmas[i] === raides[0]){
+        kiekis1 += 1;
+    }else if (nmas[i] === raides[1]){
+        kiekis2 += 1;
+    }else if (nmas[i] === raides[2]){
+        kiekis3 += 1;
+    }else if (nmas[i] === raides[3]){
+        kiekis4 += 1;
+    }
+}
+
+console.log(nmas);
+console.log(kiekis1, raides[0]);
+console.log(kiekis2, raides[1]);
+console.log(kiekis3, raides[2]);
+console.log(kiekis4, raides[3]);
+
+console.log('----------8---------');
+/*8. Parašyti funkciją - lygineSuma. Funkcijos parametrai - du kintamieji. Testų reikalavimai - abu kitamieji turi būti arba skaičiai arba masyvai(negali būti vienas skaičius, kitas masyvas).
+Jei kintamieji skaičiai, grąžinti skaičių sumą, jei kintamieji masyvai - grąžinti masyvų ilgių sumą. Jei abu kintamieji skaičiai arba masyvai, bet suma nelyginė - grąžinti tekstą, kad suma nelyginė. (10 taškų)*/
+
+
+
+
+
+
+/*9. Parašyti funkciją pirminisSkaicius. Funkcija turi vieną kintamąjį. Turi būti patikrinimas, kad kintamasis yra skaičius. Funkcija turi grąžinti ar pateiktas skaičius yra pirminis( pirminis
 skaičius yra tas, kuris dalinasi tik iš savęs ir tik iš vieneto be liekanos.) (10 taškų)
 10. Parašyti funkciją telefonoNumeris. Funkcija turi priimti vieną kintamąjį - masyvą. Masyvo elementai - skaičiai, ilgis - 10. Funkcija turi grąžinti telefono numerį tokiu formatu - 
 "(XXX) XXX-XXXX". (10 taškų)*/
